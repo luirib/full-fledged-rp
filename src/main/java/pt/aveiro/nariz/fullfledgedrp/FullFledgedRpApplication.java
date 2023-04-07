15d21b1bbb454d3333c3a18e316c4a4f2cc1b2f4
Luis Ribeiro <luis.ribeiro@swisssign.com>
2023-04-07 10:30:19 +0200

    Spring boot setup

---
+++ src/main/java/pt/aveiro/nariz/fullfledgedrp/FullFledgedRpApplication.java
@@ -0,0 +1,13 @@
+package pt.aveiro.nariz.fullfledgedrp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullFledgedRpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullFledgedRpApplication.class, args);
+    }
+
+}
