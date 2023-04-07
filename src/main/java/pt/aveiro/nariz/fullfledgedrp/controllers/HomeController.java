e3ecc7b3ec60ed277e787efddfbe228d5edb130f
Luis Ribeiro <luis.ribeiro@swisssign.com>
2023-04-07 10:38:57 +0200

    Add index html

---
+++ src/main/java/pt/aveiro/nariz/fullfledgedrp/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package pt.aveiro.nariz.fullfledgedrp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping
+    public String home() {
+        return "index";
+    }
+}
